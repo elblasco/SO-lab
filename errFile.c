b55a9ea6cf55a2425385b93953d94386eb87926f
ElBlasco69 <blascovi@outlook.it>
2022-05-02 09:20:34 +0200

    Aggiunta errFile.c

---
+++ errFile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h> //errFile.c
+extern int errno; // declare external global variable
+int main(void){
+    FILE * pf;
+    pf = fopen ("nonExistingFile.boh", "rb"); //Try to open file
+    if (pf == NULL) { //something went wrong!
+        fprintf(stderr, "errno = %d\n", errno);
+        perror("Error printed by perror");
+        fprintf(stderr,"Strerror: %s\n", strerror(errno));
+    }
+    else {
+        fclose (pf);
+    }
+}
