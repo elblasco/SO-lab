ef96d8d69671d46fe8ca830ed43e5045b98f3aff
ElBlasco69 <blascovi@outlook.it>
2022-05-02 09:38:16 +0200

    Aggiunti input.c e output.c

---
+++ output.c
@@ -0,0 +1,10 @@
+// output.out
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+    for (int i = 0; i<3; i++) {
+        sleep(2);
+        fprintf(stdout,"Written in buffer");
+        fflush(stdout);
+    };
+};
