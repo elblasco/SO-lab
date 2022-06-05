068741b084744061bc0b23f9fa9b8db7a2fd824a
ElBlasco69 <blascovi@outlook.it>
2022-06-05 16:09:53 +0200

    Sesso

--- similEsame/primaSimulazione/main.c
+++ similEsame/primaSimulazione/main.c
@@ -1,66 +1,113 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdbool.h> 
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include <sys/wait.h>
+#include<string.h>
 
-#define MAXARG 3
+#define MAXLEAF 10
 
-FILE* file_out;
+FILE*out;
+int foglie[MAXLEAF],indice_foglie=0,fd[2];
 
-bool file_exists(const char* path){
-    FILE* tmp=fopen(path,"r");
-    bool trovato=false;
-    if(tmp!=NULL){
-        trovato=true;
-        fclose(tmp);
-    }
-    return trovato;
-}
-
-void stampa_pid(){
-    fprintf(file_out,"%d\n",getpid());
-}
-
-void fai_il_manager(int n_filgi){
-    stampa_pid();
-    for(int i=0;i<n_filgi;++i){
-        int n_esimo_figlio=fork();
-        if(n_esimo_figlio==0){
-            stampa_pid();
-            pause();
+void manager_handler(int signo){
+    if(signo==SIGUSR1){
+        if(indice_foglie-1<0){
+            exit(0);
         }
         else{
+            printf("Faccio sesso\n");
+            fflush(stdout);
+            close(fd[0]);
+            write(fd[1],"SIGUSR1",8);
+            write(fd[1],getpid(),sizeof(int)*5);
+            printf("Mo uccido la figlia con il PID %d\n",foglie[indice_foglie-1]);
+            fflush(stdout);
+            kill(SIGKILL,foglie[indice_foglie-1]);
+            indice_foglie--;
+        }
+    }
+    if(signo==SIGUSR2){
+        printf("Mi è arrivato uno SIGUSR2 da una foglia\n");
+        fflush(stdout);
+    }
+}
 
+bool file_check(const char* file_name){
+    FILE* tmp=fopen(file_name,"r");
+    bool find=false;
+    if(tmp!=NULL){
+        find=true;
+        fclose(tmp);
+    }
+    return find;
+}
+
+void scrivi_pid(){
+    fprintf(out,"%d\n",getpid());
+    fflush(out);
+}
+
+int genera_foglie(int n_foglie){
+    for(int i=0;i<n_foglie;++i){
+        int foglia=fork();
+        foglie[indice_foglie]=foglia;
+        indice_foglie++;
+        if(foglia==0){
+            scrivi_pid();
+            char lettura_pipe[8];
+            int pid_manager=0;
+            close(fd[1]);
+            read(fd[0],pid_manager,sizeof(int)*5);
+            read(fd[0],lettura_pipe,8);
+            if(strcmp("SIGUSR1",lettura_pipe)){
+                kill(SIGUSR2,pid_manager);
+            }
+            while(true){
+                pause();
+            }
         }
     }
 }
 
-int main(int argc, char ** argv){
-    if(argc!=MAXARG){
-        perror("Troppi argomenti");
+int logica_manager(int i){
+    signal(SIGUSR1,manager_handler);
+    signal(SIGUSR2,manager_handler);
+    printf("Il manager ha PID %d\n",getpid());
+    scrivi_pid();
+    if(pipe(fd)<0){
+        exit(-66);
+    }
+    genera_foglie(i);
+    while(wait(NULL)>0);
+    return 1;
+}
+
+int main(int argc ,char ** argv){
+    int n=atoi(argv[2]);
+    if(argc!=3){
         exit(3);
     }
-    if(atoi(argv[2])<1 || atoi(argv[2])>10){
-        perror("n non compreso tra 1 e 10");
+    if(n<1 || n>10){
         exit(4);
     }
-    
-    if(file_exists(argv[1])){
-        perror("Errore nell'apertura del file");
+    if(file_check(argv[1])){
         exit(5);
     }
-    file_out=fopen(argv[1],"w");
-    if(file_out==NULL){
-        perror("Errore nell'apertura del file");
+    out=fopen(argv[1],"w");
+    if(out==NULL){
         exit(5);
     }
-    stampa_pid();
+    scrivi_pid();
     int manager=fork();
     if(manager==0){
-        fai_il_manager(atoi(argv[2]));
+        //printf("Hey sono il manager il cui pid è %d\n",getpid());
+        return logica_manager(n);
     }
     else{
-
+        //printf("Il padre aspetta e ha il pid %d\n",getpid());
+        while(wait(NULL)>0);
     }
     return 0;
 }
